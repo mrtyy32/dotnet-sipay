f72fe4a89ba021cb9eaf259b793b111a3aefd8d7
Murat Savran <mrtyy32@yandex.com>
2022-06-25 14:07:27 +0300

    refact

--- src/Sipay/Helper.cs
+++ src/Sipay/Helper.cs
@@ -4,48 +4,55 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
-
 namespace Sipay
 {
     class Helper
     {
-        public static string GenerateHashKey(string total, string installment, string currency_code,
-            string merchant_key, string invoice_id, string app_secret)
+        public static string GenerateHashKey(string total, string installment, string currencyCode,
+            string merchantKey, string invoiceId, string appSecret)
         {
-            var data = total + '|' + installment + '|' + currency_code + '|' + merchant_key + '|' + invoice_id;
+            var data = total + '|' + installment + '|' + currencyCode + '|' + merchantKey + '|' + invoiceId;
             var sha1 = new SHA1Managed();
-            Random rnd = new Random();
+            
+            var rnd = new Random();
             var iv = sha1.ComputeHash(Encoding.UTF8.GetBytes(rnd.ToString()));
-            var password = sha1.ComputeHash(Encoding.UTF8.GetBytes(app_secret)).ToString();
+            var password = sha1.ComputeHash(Encoding.UTF8.GetBytes(appSecret)).ToString();
+            
             rnd = new Random();
             var shaSalt = sha1.ComputeHash(Encoding.UTF8.GetBytes(rnd.ToString())).ToString();
             var salt = shaSalt.Substring(0, 4);
 
             var sha256 = new SHA256Managed();
             var saltWithPassword = sha256.ComputeHash(Encoding.UTF8.GetBytes(password + salt));
-            //var encrypted = EncryptString(data,saltWithPassword,iv);
-            var encrypted = "asdasdasd";
-            var msg_encrypted_bundle = iv + ":" + salt + ":" + encrypted;
-            msg_encrypted_bundle = msg_encrypted_bundle.Replace("/", "__");
-            return msg_encrypted_bundle;
+            var encrypted = EncryptString(data,saltWithPassword,iv);
+            
+            var msgEncryptedBundle = iv + ":" + salt + ":" + encrypted;
+            msgEncryptedBundle = msgEncryptedBundle.Replace("/", "__");
+            
+            return msgEncryptedBundle;
         }
 
         public static string EncryptString(string plainText, byte[] key, byte[] iv)
         {
-            Aes encryptor = Aes.Create();
+            var encryptor = Aes.Create();
             encryptor.Mode = CipherMode.CBC;
             encryptor.Key = key;
             encryptor.IV = iv;
-            MemoryStream memoryStream = new MemoryStream();
-            ICryptoTransform aesEncryptor = encryptor.CreateEncryptor();
-            CryptoStream cryptoStream = new CryptoStream(memoryStream, aesEncryptor, CryptoStreamMode.Write);
-            byte[] plainBytes = Encoding.ASCII.GetBytes(plainText);
+
+            var memoryStream = new MemoryStream();
+            var aesEncryptor = encryptor.CreateEncryptor();
+            var cryptoStream = new CryptoStream(memoryStream, aesEncryptor, CryptoStreamMode.Write);
+            var plainBytes = Encoding.ASCII.GetBytes(plainText);
+            
             cryptoStream.Write(plainBytes, 0, plainBytes.Length);
             cryptoStream.FlushFinalBlock();
-            byte[] cipherBytes = memoryStream.ToArray();
+            
+            var cipherBytes = memoryStream.ToArray();
+            
             memoryStream.Close();
             cryptoStream.Close();
-            string cipherText = Convert.ToBase64String(cipherBytes, 0, cipherBytes.Length);
+            
+            var cipherText = Convert.ToBase64String(cipherBytes, 0, cipherBytes.Length);
             return cipherText;
         }
 
--- src/Sipay/SipayClient.cs
+++ src/Sipay/SipayClient.cs
@@ -3,113 +3,116 @@ using System.Collections.Generic;
 
 namespace Sipay
 {
-	public class SipayClient : ISipayBuilder
-	{
-		private SipayRequest request;
+    public class SipayClient : ISipayBuilder
+    {
+        private SipayRequest request;
 
-		private string TEST_MERCHANT_KEY = "$2y$10$HmRgYosneqcwHj.UH7upGuyCZqpQ1ITgSMj9Vvxn.t6f.Vdf2SQFO";
-		private string TEST_APP_KEY = "6d4a7e9374a76c15260fcc75e315b0b9";
-		private string TEST_SECRET_KEY ="b46a67571aa1e7ef5641dc3fa6f1712a";
-		private string TEST_URL = "https://provisioning.sipay.com.tr/ccpayment/api";
-		private string PROD_URL = "https://app.sipay.com.tr/ccpayment/api/";
-		private string BASE_URL;
-		private string TOKEN_URL =$"/token";
-		private string PAYMENT_URL = $"/paySmart3D";
+        private string TEST_MERCHANT_KEY = "$2y$10$HmRgYosneqcwHj.UH7upGuyCZqpQ1ITgSMj9Vvxn.t6f.Vdf2SQFO";
+        private string TEST_APP_KEY = "6d4a7e9374a76c15260fcc75e315b0b9";
+        private string TEST_SECRET_KEY = "b46a67571aa1e7ef5641dc3fa6f1712a";
+        private string TEST_URL = "https://provisioning.sipay.com.tr/ccpayment/api";
+        private string PROD_URL = "https://app.sipay.com.tr/ccpayment/api/";
+        private string BASE_URL;
+        private string TOKEN_URL = $"/token";
+        private string PAYMENT_URL = $"/paySmart3D";
 
-		public SipayClient(bool test = false)
-		{
-			this.Test(test);
-		}
+        public SipayClient(bool test = false)
+        {
+            this.Test(test);
+        }
 
-		public ISipayBuilder CreditCard(string cardholderName, string creditCardNumber, string expiryMonth, string expiryYear, string cvv)
-		{
-			request.CardholderName = cardholderName;
-			request.CreditCardNumber = creditCardNumber;
-			request.ExpiryMonth = expiryMonth;
-			request.ExpiryYear = expiryYear;
-			request.CVV = cvv;
+        public ISipayBuilder CreditCard(string cardholderName, string creditCardNumber, string expiryMonth,
+            string expiryYear, string cvv)
+        {
+            request.CardholderName = cardholderName;
+            request.CreditCardNumber = creditCardNumber;
+            request.ExpiryMonth = expiryMonth;
+            request.ExpiryYear = expiryYear;
+            request.CVV = cvv;
 
-			return this;
-		}
-		
-		public ISipayBuilder Customer(string firstName, string lastName, string ipAddress)
-		{
-			request.FirstName = firstName;
-			request.LastName = lastName;
+            return this;
+        }
 
-			return this;
-		}
-		
-		public ISipayBuilder BillingAddress(string address1, string address2, string state, string city, string country, string zipCode, string email, string phone)
-		{
-			request.BillingAddress1 = address1;
-			request.BillingAddress2 = address2;
-			request.BillingState = state;
-			request.BillingCity = city;
-			request.BillingCountry = country;
-			request.BillingEmail = email;
-			request.BillingPhone = phone;
-			request.BillingZipCode = zipCode;
+        public ISipayBuilder Customer(string firstName, string lastName, string ipAddress)
+        {
+            request.FirstName = firstName;
+            request.LastName = lastName;
 
-			return this;
-		}
-		
-		public ISipayBuilder Order(string currencyCode, int installments, string orderGuid, double orderTotal)
-		{
-			request.CurrencyCode = currencyCode;
-			request.Installments = installments;
-			request.OrderGuid = orderGuid;
-			request.Description = $"{orderGuid} nolu sipariş ödemesi";
-			request.OrderTotal = orderTotal;
+            return this;
+        }
 
-			return this;
-		}
-		
-		public ISipayBuilder BasketItems(List<SipayBasketItem> basketItems)
-		{
-			var itemJsonText = basketItems.ToJson();
+        public ISipayBuilder BillingAddress(string address1, string address2, string state, string city, string country,
+            string zipCode, string email, string phone)
+        {
+            request.BillingAddress1 = address1;
+            request.BillingAddress2 = address2;
+            request.BillingState = state;
+            request.BillingCity = city;
+            request.BillingCountry = country;
+            request.BillingEmail = email;
+            request.BillingPhone = phone;
+            request.BillingZipCode = zipCode;
 
-			request.Items = itemJsonText;
-			return this;
-		}
+            return this;
+        }
 
-		public ISipayBuilder Callbacks(string returlUrl, string cancelUrl)
-		{
-			request.ReturnUrl = returlUrl;
-			request.CancelUrl = cancelUrl;
+        public ISipayBuilder Order(string currencyCode, int installments, string orderGuid, double orderTotal)
+        {
+            request.CurrencyCode = currencyCode;
+            request.Installments = installments;
+            request.OrderGuid = orderGuid;
+            request.Description = $"{orderGuid} nolu sipariş ödemesi";
+            request.OrderTotal = orderTotal;
 
-			return this;
-		}
+            return this;
+        }
 
-		public ISipayBuilder Execute(string token = "")
-		{
-			if (string.IsNullOrEmpty(token))
-			{
-				this.GetToken();
-				return this;
-			}
-			return this;
-		}
+        public ISipayBuilder BasketItems(List<SipayBasketItem> basketItems)
+        {
+            var itemJsonText = basketItems.ToJson();
 
-		public ISipayBuilder Test(bool isTest = true)
-		{
-			if (isTest)
-				this.BASE_URL = TEST_URL;
-			else this.BASE_URL = PROD_URL;
+            request.Items = itemJsonText;
+            return this;
+        }
 
-			return this;
-		}
+        public ISipayBuilder Callbacks(string returlUrl, string cancelUrl)
+        {
+            request.ReturnUrl = returlUrl;
+            request.CancelUrl = cancelUrl;
 
-		public ISipayBuilder Settings(string appId, string appSecret, string merchantKey)
-		{
-			//TODO
-			return this;
-		}
+            return this;
+        }
 
-		private string GetToken()
-		{
-			//TODO
-			return "";
-		}
-	}
+        public ISipayBuilder Execute(string token = "")
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                this.GetToken();
+                return this;
+            }
+
+            return this;
+        }
+
+        public ISipayBuilder Test(bool isTest = true)
+        {
+            if (isTest)
+                this.BASE_URL = TEST_URL;
+            else this.BASE_URL = PROD_URL;
+
+            return this;
+        }
+
+        public ISipayBuilder Settings(string appId, string appSecret, string merchantKey)
+        {
+            //TODO
+            return this;
+        }
+
+        private string GetToken()
+        {
+            //TODO
+            return "";
+        }
+    }
 }
